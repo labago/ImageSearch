e1bb0f5eb592f5cade8ac4d3453da01aa9eceebe
Jonathan Lane <jlane09sjp@gmail.com>
2013-02-01 16:33:43 -0500

    added two class files, just to structure things a bit

---
+++ ImageSearch.py
@@ -0,0 +1,13 @@
+from SearchImage import *
+
+class ImageSearch:
+	pattern_images = []
+	specimen_images = []
+
+	def match_images(self, patterns, specimens):
+		return 0
+
+
+	def match_image(self, pattern, specimen):
+		return 0
+
---
+++ SearchImage.py
@@ -0,0 +1,8 @@
+class SearchImage:
+	width = 0
+	height = 0
+	size = 0
+	imageType = ""
+
+	def details(self):
+		return 0
