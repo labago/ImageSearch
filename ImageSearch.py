09203d835767cb8e50111e66f9808a1f529778a0
Jonathan Lane <jlane09sjp@gmail.com>
2013-02-09 18:44:46 -0500

    Revampled the key point match function to reflect Mikes feedback, works with 1 to 1 images

--- ImageSearch.py
+++ ImageSearch.py
@@ -37,76 +37,43 @@ class ImageSearch:
 		else:
 			print "Does Not Match", percentage*100, "percent"
 
-	def key_point_match(self, pattern, source):
+	def key_point_match(self, pattern, source, uniques):
 		patternPixels = pattern.load()
 		sourcePixels = source.load()
-
-		patWidth = pattern.size[0]
-		patHeight = pattern.size[1]
-
-		Xinterval = int((patWidth/10))-1
-		Yinterval = int((patHeight/10))-1
 
 		sourceWidth = source.size[0]
 		sourceHeight = source.size[1]
 
-		patPixelArray = []
-		
-		# Pattern Image
-		# get very top row of pattern image 
-		for x in range(0,patWidth, Xinterval):
-			patPixelArray.append(patternPixels[x,0])
+		sourcePixelArray = []
 
-		# get very bottom pattern image 
-		for x in range(0,patWidth, Xinterval):
-			patPixelArray.append(patternPixels[x,patHeight-1])
+		print "Getting source pixel array..."
+		for x in range(0,sourceWidth):
+			for y in range(0, sourceHeight):
+				sourcePixelArray.append((sourcePixels[x,y], x, y))
 
-		# get very left column of pattern image
-		for y in range(0,patHeight, Yinterval):
-			patPixelArray.append(patternPixels[0,y])
+		print "Trying to match unique pixels with source image..."
+		for x in range(0, len(uniques)):
+			if self.is_pixel_in_source(uniques[x], sourcePixelArray):
+				source_coordinates = self.find_unique_in_source(uniques[x], sourcePixelArray)
+				pattern_xc = uniques[x][1]
+				pattern_yc = uniques[x][2]
 
-		# get very right column of pattern image
-		for y in range(0,patHeight, Yinterval):
-			patPixelArray.append(patternPixels[patWidth-1,y])
+				source_xc = source_coordinates[0]
+				source_yc = source_coordinates[1]
 
-		# make source pixel array as many times as needed until a match is found or
-		# until we run out of places to try the pattern image against it. 
-		# it starts at x=0 and y=0, moving down in the y direction as far as it can before
-		# moving over the next x pixel and starting over. Not very optimized but its something.
-		column = 0
-		row = 0
-		for x in range(0,sourceWidth-patWidth, Xinterval):
-			column += 1
-			print "Column:", column
-			for y in range(0, sourceHeight-patHeight, Yinterval):
-				row += 1
-				print "Row:", row
-				# Source Image
-				sourcePixelArray = []
-				# get very top row of source image 
-				for xx in range(x, patWidth, Xinterval):
-					sourcePixelArray.append(sourcePixels[xx,x])
+				x_offset = source_xc - pattern_xc
+				y_offset = source_yc - pattern_yc
 
-				# get very bottom pattern image 
-				for xx in range(x, patWidth, Xinterval):
-					sourcePixelArray.append(sourcePixels[xx,patHeight-1])
+				print "X Offset:", x_offset
+				print "Y Offset:", y_offset
 
-				# get very left column of pattern image
-				for yy in range(y, patHeight, Yinterval):
-					sourcePixelArray.append(sourcePixels[y,yy])
-
-				# get very right column of pattern image
-				for yy in range(y, patHeight, Yinterval):
-					sourcePixelArray.append(sourcePixels[patWidth-1,yy])
-
-				percentage = self.array_match_percentage(patPixelArray, sourcePixelArray)
+				percentage = self.percentage_of_unique_matches(uniques, sourcePixelArray, x_offset, y_offset)
 
 				if(percentage > .5):
-					return "MATCHES", percentage*100, "percent."
-				else:
-					print "Does Not Match", percentage*100, "percent"
+					return "MATCHES!!!", percentage*100, "percent."
 
-		return "NO MATCH FOUND"
+		return "Does Not Match!"
+
 
 	def array_match_percentage(self, pattern, source):
 		matches = 0.00
@@ -119,6 +86,63 @@ class ImageSearch:
 
 		return matches/(len(pattern)+0.00)
 
+	def find_unique_pixels(self, pattern):
+		print "Getting unique pixel array..."
+
+		patternPixels = pattern.load()
+		patSize = pattern.size
+
+		patPixelArray = []
+		uniques = []
+
+		for x in range(0,patSize[0]):
+			for y in range(0, patSize[1]):
+				patPixelArray.append((patternPixels[x,y], x, y))
+
+		print "Original # of pattern pic pixels:", len(patPixelArray)
+
+		for x in range(0, len(patPixelArray)):
+			if self.is_unique_pixel(patPixelArray[x], patPixelArray):
+				uniques.append(patPixelArray[x])
+
+		print len(uniques), "unique pixels found"
+		return uniques
+
+
+	def is_unique_pixel(self, pixel, array):
+		for x in range(0, len(array)):
+			if array[x][0] == pixel[0] and array[x][1] != pixel[1] and array[x][2] != pixel[2]:
+				return False
+		return True
+
+	def is_pixel_in_source(self, pixel, array):
+		for x in range(0, len(array)):
+			if array[x][0] == pixel[0]:
+				return True
+		return False
+
+	def find_unique_in_source(self, pixel, array):
+		for x in range(0, len(array)):
+			if array[x][0] == pixel[0]:
+				return (array[x][1], array[x][2])
+		return False
+
+	def percentage_of_unique_matches(self, uniques, source, x_offset, y_offset):
+		matches = 0.00
+
+		for x in range(0, len(uniques)):
+			if self.is_pixel_in_source(uniques[x], source):
+				source_coordinates = self.find_unique_in_source(uniques[x], source)
+				pattern_xc = uniques[x][1]+x_offset
+				pattern_yc = uniques[x][2]+y_offset
+
+				source_xc = source_coordinates[0]
+				source_yc = source_coordinates[1]
+
+				if pattern_xc == source_xc and pattern_yc == source_yc:
+					matches += 1.00
+
+		return matches/(len(uniques)+0.00)
 
 
 imageSearch = ImageSearch(str(sys.argv[1]), str(sys.argv[2]))
@@ -127,5 +151,5 @@ imageSearch = ImageSearch(str(sys.argv[1]), str(sys.argv[2]))
 # print 'Source Image Info: ', sourceSize[0]
 
 #print imageSearch.key_point_match(imageSearch.pattern_image, imageSearch.source_image)
-print imageSearch.key_point_match(imageSearch.pattern_image, imageSearch.source_image)
+print imageSearch.key_point_match(imageSearch.pattern_image, imageSearch.source_image, imageSearch.find_unique_pixels(imageSearch.pattern_image))
 
