f6ae4a67efe668db1352515270220b08e510454d
Jonathan Lane <jlane09sjp@gmail.com>
2013-02-09 22:00:35 -0500

    added another test crop image that doesnt start from the left edge

--- crop.py
+++ crop.py
@@ -4,4 +4,4 @@ import sys
 the_image = Image.open(sys.argv[1])
 
 w,h = the_image.size
-the_image.crop((20, 20, 200, 400)).save('croppedImage.png')
+the_image.crop((100, 100, 200, 400)).save('croppedImage.png')
